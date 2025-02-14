b6f2ebf7d8d9139d19a943bd1494ddd9b0867d1a
DmitryV13 <voiku173dmitry@gmail.com>
2025-02-14 11:31:55 +0200

    backend was added

---
+++ back/src/main/java/com/mycompany/myapp/domain/Task.java
@@ -0,0 +1,114 @@
+package com.mycompany.myapp.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+
+/**
+ * A Task.
+ */
+@Entity
+@Table(name = "task")
+@SuppressWarnings("common-java:DuplicatedBlocks")
+public class Task implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "status")
+    private Boolean status;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public Task id(Long id) {
+        this.setId(id);
+        return this;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public Task title(String title) {
+        this.setTitle(title);
+        return this;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Task description(String description) {
+        this.setDescription(description);
+        return this;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getStatus() {
+        return this.status;
+    }
+
+    public Task status(Boolean status) {
+        this.setStatus(status);
+        return this;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Task)) {
+            return false;
+        }
+        return getId() != null && getId().equals(((Task) o).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+        return getClass().hashCode();
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "Task{" +
+            "id=" + getId() +
+            ", title='" + getTitle() + "'" +
+            ", description='" + getDescription() + "'" +
+            ", status='" + getStatus() + "'" +
+            "}";
+    }
+}
---
+++ back/src/main/java/com/mycompany/myapp/service/TaskService.java
@@ -0,0 +1,110 @@
+package com.mycompany.myapp.service;
+
+import com.mycompany.myapp.domain.Task;
+import com.mycompany.myapp.repository.TaskRepository;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Service Implementation for managing {@link com.mycompany.myapp.domain.Task}.
+ */
+@Service
+@Transactional
+public class TaskService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TaskService.class);
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    /**
+     * Save a task.
+     *
+     * @param task the entity to save.
+     * @return the persisted entity.
+     */
+    public Task save(Task task) {
+        LOG.debug("Request to save Task : {}", task);
+        return taskRepository.save(task);
+    }
+
+    /**
+     * Update a task.
+     *
+     * @param task the entity to save.
+     * @return the persisted entity.
+     */
+    public Task update(Task task) {
+        LOG.debug("Request to update Task : {}", task);
+        return taskRepository.save(task);
+    }
+
+    /**
+     * Partially update a task.
+     *
+     * @param task the entity to update partially.
+     * @return the persisted entity.
+     */
+    public Optional<Task> partialUpdate(Task task) {
+        LOG.debug("Request to partially update Task : {}", task);
+
+        return taskRepository
+            .findById(task.getId())
+            .map(existingTask -> {
+                if (task.getTitle() != null) {
+                    existingTask.setTitle(task.getTitle());
+                }
+                if (task.getDescription() != null) {
+                    existingTask.setDescription(task.getDescription());
+                }
+                if (task.getStatus() != null) {
+                    existingTask.setStatus(task.getStatus());
+                }
+
+                return existingTask;
+            })
+            .map(taskRepository::save);
+    }
+
+    /**
+     * Get all the tasks.
+     *
+     * @param pageable the pagination information.
+     * @return the list of entities.
+     */
+    @Transactional(readOnly = true)
+    public Page<Task> findAll(Pageable pageable) {
+        LOG.debug("Request to get all Tasks");
+        return taskRepository.findAll(pageable);
+    }
+
+    /**
+     * Get one task by id.
+     *
+     * @param id the id of the entity.
+     * @return the entity.
+     */
+    @Transactional(readOnly = true)
+    public Optional<Task> findOne(Long id) {
+        LOG.debug("Request to get Task : {}", id);
+        return taskRepository.findById(id);
+    }
+
+    /**
+     * Delete the task by id.
+     *
+     * @param id the id of the entity.
+     */
+    public void delete(Long id) {
+        LOG.debug("Request to delete Task : {}", id);
+        taskRepository.deleteById(id);
+    }
+}
